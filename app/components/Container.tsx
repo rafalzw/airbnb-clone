4f1c32e2df43594ef57a1393b8040af7f33bf673
Rafal Zw <zwierz.rafal@gmail.com>
2023-10-19 19:54:01 +0200

    App Search component.

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -3,22 +3,24 @@
 import React from 'react';
 
 interface ContainetProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const Container: React.FC<ContainetProps> = ({children}) => {
-    return (
-        <div className='
-        max-w-[2520px]
-        mx-auto
-        xl:px-20
-        md:px-10
-        sm:px-2
-        px-4
-        '>
-            {children}
-        </div>
-    );
+const Container: React.FC<ContainetProps> = ({ children }) => {
+  return (
+    <div
+      className='
+       max-w-[2520px]
+       mx-auto
+       xl:px-20
+       md:px-10
+       sm:px-2
+       px-4
+      '
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import Container from "@/app/components/Container";
-import Logo from "@/app/components/navbar/Logo";
+import Container from '../Container';
+import Logo from './Logo';
+import Search from './Search';
 
 function Navbar() {
-    return (
-        <div className='fixed w-full bg-white z-10 shadow-sm'>
-            <div className='
-            py-4
-            border-b-[1px]
+  return (
+    <div
+      className='
+        fixed
+        w-full
+        bg-white
+        z-10
+        shadow-sm
+        '>
+      <div
+        className='
+          py-4
+          border-b-[1px]
             '>
-                <Container>
-                    <div className='
-                    flex
-                    flex-row
-                    items-center
-                    justify-between
-                    gap-3
-                    md:gap-0
-                    '>
-                        <Logo/>
-                    </div>
-                </Container>
-            </div>
-        </div>
-    );
+        <Container>
+          <div
+            className='
+              flex
+              flex-row
+              items-center
+              justify-between
+              gap-3
+              md:gap-0
+            '
+          >
+            <Logo />
+            <Search />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
