76c3e2db52cbe13c3e7199571c16d9bcfae0797a
Rafal Zw <zwierz.rafal@gmail.com>
2023-10-19 23:44:32 +0200

    Add toggleOpen function to UserMenu.

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,17 @@
 'use client';
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen(value => !value);
+  }, []);
+
   return (
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
@@ -27,7 +34,7 @@ const UserMenu = () => {
           Wynajmij swój dom w Airbnb
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className='
             p-4
             md:py-1
@@ -44,12 +51,42 @@ const UserMenu = () => {
             transition
           '
         >
-          <AiOutlineMenu/>
+          <AiOutlineMenu />
           <div className='hidden md:block'>
-            <Avatar/>
+            <Avatar />
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div
+          className='
+            absolute
+            rounded-xl
+            shadow-md
+            w-[40vw]
+            md:w-3/4
+            bg-white
+            overflow-hidden
+            right-0
+            top-12
+            text-sm
+            '
+        >
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem
+                onClick={() => {}}
+                label='Zarejestruj'
+              />
+              <MenuItem
+                onClick={() => {}}
+                label='Zaloguj'
+              />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
