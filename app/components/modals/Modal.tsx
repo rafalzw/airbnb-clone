cbbec87fee4506c22f61cd560f61b15312ecb4bb
Rafal Zw <zwierz.rafal@gmail.com>
2023-10-20 23:43:03 +0200

    Add Button component.

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useCallback, useEffect, useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
+import Button from '../Button';
 
 interface ModalProps {
   isOpen: boolean;
@@ -166,6 +167,7 @@ const Modal: React.FC<ModalProps> = (
                     w-full
                   '
                 >
+                  <Button label='Test' />
                 </div>
               </div>
             </div>
