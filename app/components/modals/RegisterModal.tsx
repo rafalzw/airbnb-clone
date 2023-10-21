a49cdf7fd65f29aca68267bec194bdc28d636f4b
Rafal Zw <zwierz.rafal@gmail.com>
2023-10-21 09:57:54 +0200

    Add RegisterModal component.

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
+import { FcGoogle } from 'react-icons/fc';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modal from '@/app/components/modals/Modal';
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors,
+    },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios.post('/api/register', data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title='Rejestracja'
+      actionLabel='Continue'
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
+};
+
+export default RegisterModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
-import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang='en'>
     <body className={font.className}>
     <ClientOnly>
-      <Modal actionLabel='Submit' isOpen title='Login' />
+      <RegisterModal />
       <Navbar />
     </ClientOnly>
     {children}</body>
