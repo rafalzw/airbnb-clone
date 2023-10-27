b20d0b545ab4adfc85dc8ed7256b88018d4c569f
Rafal Zw <zwierz.rafal@gmail.com>
2023-10-27 17:32:33 +0200

    Implement SingOut functionality.

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,6 +7,7 @@ import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { SafeUser } from '@/app/types';
+import { signOut } from 'next-auth/react';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
@@ -106,7 +107,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   label='Konto'
                 />
                 <MenuItem
-                  onClick={() => {}}
+                  onClick={() => signOut()}
                   label='Wyloguj'
                 />
               </>
