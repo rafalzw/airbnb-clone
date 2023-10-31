c0905c7c581bedf9d91ded4f683dfab1e3f48d3c
Rafal Zw <zwierz.rafal@gmail.com>
2023-10-31 19:12:36 +0100

    Fix selected item.

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -5,6 +5,7 @@ import Container from '../Container';
 import { TbBeach, TbPool } from 'react-icons/tb';
 import { MdOutlineVilla } from 'react-icons/md';
 import CategoryBox from '../CategoryBox';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export const categories = [
   {
@@ -26,6 +27,16 @@ export const categories = [
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+
+  const isMainPage = pathname === '/';
+
+  if (!isMainPage) {
+    return null;
+  }
+
   return (
     <Container>
       <div
@@ -42,7 +53,7 @@ const Categories = () => {
           <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category === item.label}
             icon={item.icon}
           />
         ))}
