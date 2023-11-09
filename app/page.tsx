c62a5f02c763e705ecf2d10970b91832b05eb866
Rafal Zw <zwierz.rafal@gmail.com>
2023-11-09 22:16:05 +0100

    Add EmptyState component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
+import ClientOnly from './components/ClientOnly';
+import Container from './components/Container';
+import EmptyState from './components/EmptyState';
+
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div
-      className='
-        text-red-500 
-        text-2xl'
-    >
-      Hello Airbnb!
-    </div>
+    <ClientOnly>
+      <Container>
+        <div
+          className='
+              pt-24
+              grid
+              grid-cols-1
+              sm:grid-cols-2
+              md:grid-cols-3
+              lg:grid-cols-4
+              xl:grid-cols-5
+              2xl:grid-cols-6
+              gap-8
+        '
+        >
+          <div>Moje ogłoszenie</div>
+        </div>
+      </Container>
+    </ClientOnly>
   );
 }
