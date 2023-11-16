d0c832875e5214ba7250143baa5017cb534670dd
Rafal Zw <zwierz.rafal@gmail.com>
2023-11-16 10:21:52 +0100

    Fix listings warning.

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,8 +1,12 @@
-import { User } from '.prisma/client';
+import { Listing, User } from '.prisma/client';
+
+export type SafeListings = Omit<Listing, 'createdAt'> & {
+  createdAt: string;
+};
 
 export type SafeUser = Omit<
   User,
-  "createdAt" | "updatedAt" | "emailVerified"
+  'createdAt' | 'updatedAt' | 'emailVerified'
 > & {
   createdAt: string;
   updatedAt: string;
