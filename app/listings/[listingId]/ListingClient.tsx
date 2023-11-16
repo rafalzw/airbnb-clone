0c12ba76888363477bc740cc5183a4aa86bd52ca
Rafal Zw <zwierz.rafal@gmail.com>
2023-11-16 22:07:26 +0100

    Add ListingInfo component.

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,9 +2,11 @@
 
 import Container from '@/app/components/Container';
 import ListingHead from '@/app/components/listings/ListingHead';
+import ListingInfo from '@/app/components/listings/ListingInfo';
 import { categories } from '@/app/components/navbar/Categories';
 import { SafeListings, SafeUser } from '@/app/types';
 import { Reservation } from '@prisma/client';
+import { list } from 'postcss';
 import { useMemo } from 'react';
 
 interface ListingClientProps {
@@ -34,6 +36,25 @@ const ListingClient: React.FC<ListingClientProps> = ({
             id={listing.id}
             currentUser={currentUser}
           />
+          <div
+            className='
+          grid
+          grid-cols-1
+          md:grid-cols-7
+          md:gap-10
+          mt-6
+          '
+          >
+            <ListingInfo
+              user={listing.user}
+              category={listing.category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
